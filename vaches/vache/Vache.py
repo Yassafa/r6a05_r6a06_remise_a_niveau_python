f5912e50950b174a37cda77f62d68f858e5554c9
Yassafa <yassineessafa1@gmail.com>
2026-02-05 23:20:47 +0100

    13 tests verts

--- vaches/vache/Vache.py
+++ vaches/vache/Vache.py
@@ -6,15 +6,16 @@ class Vache:
     PANSE_MAX = 50.0
     POIDS_MIN_PANSE = 2.0
     RENDEMENT_RUMINATION = 0.25
-    _NEXT_ID = 1
+    NEXT_ID = 1
 
     def __init__(self, petitNom, age, poids):
-        self._id = self._NEXT_ID
-        self._NEXT_ID += 1
+        self._id = Vache.NEXT_ID
+        Vache.NEXT_ID += 1
         self._petitNom = petitNom
         self._poids = poids
         self._panse = 0
         self._age = age
+        self._valider_etat()
 
     @property
     def nom(self):
@@ -43,12 +44,18 @@ class Vache:
         self._panse += quantite
 
     def ruminer(self):
-        self._calculer_lait(self.panse)
-        self._stocker_lait("")
-        self._post_rumination()
+        self._valider_rumination_possible()
+        panse_avant = self.panse
+        gain = self.RENDEMENT_RUMINATION * panse_avant
+        self._poids += gain
+        lait = self._calculer_lait(panse_avant)
+        self._stocker_lait(lait)
+        self._panse = 0.0
+        self._post_rumination(panse_avant, lait)
 
     def vieillir(self):
         self._age += 1
+        self._valider_etat()
 
     def _calculer_lait(self, panse_avant):
         pass
@@ -56,15 +63,26 @@ class Vache:
     def _stocker_lait(self, lait):
         pass
 
-    def _post_rumination(self):
+    def _post_rumination(self, panse_avant, lait):
         pass
 
     def _ajouter_panse(self):
         pass
 
     def _valider_rumination_possible(self):
-        pass
+        if self.panse <= 0.0:
+            raise InvalidVacheException("Panse vide")
 
     def _valider_etat(self):
         if self.nom.isspace():
-            raise InvalidVacheException
+            raise InvalidVacheException("Nom vide")
+        if self.age < 0 or self.age > self.AGE_MAX:
+            raise InvalidVacheException("Âge invalide")
+        if self.poids < 0:
+            raise InvalidVacheException("Poids négatif")
+        if self.panse < 0:
+            raise InvalidVacheException("Panse négative")
+        if self.panse > self.PANSE_MAX:
+            raise InvalidVacheException("Panse trop pleine")
+        if self.poids > self.POIDS_MAX:
+            raise InvalidVacheException("Poids trop élevé")
